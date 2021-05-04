9989e6ca8e7ccc143ed535890e8b3a39f818e629
Saravana Pandian <83231084+saravana-pandian@users.noreply.github.com>
2021-05-04 11:21:23 +0530

    Add files via upload

---
+++ partif/footer.php
@@ -0,0 +1,29 @@
+  <link rel="stylesheet" href="css/style.css">
+
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c545.png" width="50" height="30" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a style="color: blue;" href="#">SARAVANA & KRISH</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
---
+++ partif/menu.php
@@ -0,0 +1,46 @@
+<?php include('config/config.php'); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+      
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="#" title="Logo">
+                    <img src="images/logo.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="<?php echo SETURL; ?>">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SETURL; ?>categories.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SETURL; ?>foods.php">Foods</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SETURL; ?>contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
